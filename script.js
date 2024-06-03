3097e6f132f13c2bf15323e2f8c700b6a77168a8
rephiz <88085291+rephiz@users.noreply.github.com>
2024-06-03 11:45:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -30,13 +30,13 @@ function getMidnight() {
 
 const expiresAtMidnight = getMidnight();
 
-for(var i=0;i<persona.length;i++){
+/*for(var i=0;i<persona.length;i++){
         var k="";
         if(persona[i].mangiato=="false"){
             k="false";
         }else k="true";
         setCookie(persona[i].codice, k, expiresAtMidnight);
-}
+}*/
 
 document.addEventListener("keydown",invio);
 
