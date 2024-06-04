5d3bc1106a82ee16629e83e3c6efdb2b9d009e72
rephiz <88085291+rephiz@users.noreply.github.com>
2024-06-04 12:16:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ var insert = document.getElementById("insert") ;
 var j = document.getElementById("j");
 var nome = document.getElementById("nome") ;
 var sub = document.getElementById("sub") ;
-
+var wifi = document.getElementById("wifi") ;
+//aa
 setInterval(function(){ //funzione per il fuoco sulla barra input
     num.focus();
 },100);
@@ -22,9 +23,9 @@ function changeContent(codice, callback) { //call ajax
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
         callback(this.responseText);
-      }
+      }else if(this.readyState == 4 && this.status != 200) callback("errore");
     };
-    xhttp.open("GET", "https://corsproxy.io/?https%3A%2F%2Fmail.elesa.com%2Fmensa%2Ftest.php%3Fbadge%3D"+codice, true);
+    xhttp.open("GET", "https://mail.elesa.com/mensa/webservice.php?badge="+codice, true);
     xhttp.send();
   }
 
@@ -65,9 +66,11 @@ function trovacodice(name) { //vedo se un codice è già presente fra i cookie
     for(let i=0; i < ca.length; i++) {
         let c = ca[i];
         while (c.charAt(0) === ' ') c = c.substring(1, c.length);
-        if((c.substring(0,nameEQ.length)) == nameEQ) return true;
+        if((c.substring(0,nameEQ.length)) == nameEQ) {
+            return true;
+        }
     }
-    return null;
+    return false;
 }
 
 function dopoRitardo() { //faccio tornare la pagina standard
@@ -77,25 +80,30 @@ function dopoRitardo() { //faccio tornare la pagina standard
     num.style.display = "";
     bottone.style.display = "";
     insert.style.display = "";
+    wifi.style.display = "none";
 }
 
 function verde(nomeuser, codice){ //schermata quando viene registrato il pasto di un utente
     j.style.backgroundColor="green";
     setCookie(codice, "true", expiresAtMidnight);
     nome.textContent = nomeuser;
+    sub.style.color = "white";
     sub.textContent = "pasto registrato!";
     num.style.display = "none";
     bottone.style.display = "none";
     insert.style.display = "none";
+    wifi.style.display = "none";
 }
 
 function giallo(nomeuser){ //schermata quando il pasto di un utente è già stato registrato
     j.style.backgroundColor="yellow";
     nome.textContent = nomeuser;
+    sub.style.color = "rgb(0, 110, 255)";
     sub.textContent = "il tuo pasto è già stato registrato.";
     num.style.display = 'none';
     bottone.style.display = 'none'; 
     insert.style.display = 'none';
+    wifi.style.display = "none";
 }
 
 function rosso(){ //schermata quando l'utente è inesistente
@@ -106,22 +114,48 @@ function rosso(){ //schermata quando l'utente è inesistente
     insert.style.display = 'none';
 }
 
+function errore(codice){
+    j.style.backgroundColor="green";
+    setCookie("err"+codice, "true", expiresAtMidnight);
+    nome.textContent = "utente numero "+codice;
+    sub.style.color = "white";
+    sub.textContent = "pasto registrato!";
+    num.style.display = "none";
+    bottone.style.display = "none";
+    insert.style.display = "none";
+    wifi.style.display = "inline";
+}
+
 function verifica(){ //quando viene inserito il codice e viene premuto invio, verifica il codice
-    var codice = num.value;
-    num.value = "";
-    changeContent(codice, function(response) {
-        var jj=response.substring(0,1);
-        if(jj!="#"){
-            if(trovacodice(codice)==true && getCookie(codice)=="true"){
-                giallo(response);
-                setTimeout(dopoRitardo, 2500);
-            }else if(trovacodice(codice)==null || (trovacodice(codice)==true && getCookie(codice)=="false")){
-                verde(response, codice);
-                setTimeout(dopoRitardo, 2500);
+    if(num.value != ""){
+        var codice = num.value;
+        num.value = "";
+        changeContent(codice, function(response) {
+            if(response=="errore"){
+                if(trovacodice(codice)==false && trovacodice("err"+codice)==false){
+                    errore(codice);
+                    setTimeout(dopoRitardo, 3500);
+                }else if((trovacodice(codice)==true || trovacodice("err"+codice)==true) && (getCookie(codice)=="true" || getCookie("err"+codice)=="true")){
+                    giallo(codice);
+                    wifi.style.display = "inline";
+                    setTimeout(dopoRitardo, 3500);
+                }
             }
-        }else{
-            rosso();
-            setTimeout(dopoRitardo, 2500);
-        }
-    });
+            if(response!="errore"){
+                var jj=response.substring(0,1);
+                if(jj!="#"){
+                    if(trovacodice(codice)==true && getCookie(codice)=="true"){
+                        giallo(response);
+                        setTimeout(dopoRitardo, 3500);
+                    }else if(trovacodice(codice)==false || (trovacodice(codice)==true && getCookie(codice)=="false")){
+                        verde(response, codice);
+                        setTimeout(dopoRitardo, 3500);
+                    }
+                }else if(jj=="#"){
+                    rosso();
+                    setTimeout(dopoRitardo, 3500);
+                }
+            }
+        });
+    }
 }
